59099077d83e8a58b19c0edd49306353c2f3cbac
Nimesh Pareek <72460673+nimeshpareek@users.noreply.github.com>
2023-09-01 17:15:08 +0530

    checking cmd line

--- polls/views.py
+++ polls/views.py
@@ -41,6 +41,7 @@ class IndexView(generic.ListView):
 
 class DetailView(generic.DetailView):
     model = Question
+    
     template_name = "polls/detail.html"
 
 # def results(request, question_id):
