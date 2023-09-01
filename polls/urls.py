2a471cb4bafdacc462f72e8ce1dba54cf745319a
Nimesh Pareek <72460673+nimeshpareek@users.noreply.github.com>
2023-09-01 17:13:03 +0530

    checking cmd line

--- polls/urls.py
+++ polls/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path
 
-from . import views as vw
+from . import views
 
 app_name = "polls"
 urlpatterns = [
-    # ex: /polls/
-    path("", vw.index, name="index"),
-    # ex: /polls/5/
-    path("<int:question_id>/", vw.detail, name="detail"),
-    # ex: /polls/5/results/
-    path("<int:question_id>/results/", vw.results, name="results"),
-    # ex: /polls/5/vote/
-    path("<int:question_id>/vote/", vw.vote, name="vote"), 
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
