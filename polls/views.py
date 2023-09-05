23dc57432746c7bfa953900779ddd703009da077
Nimesh Pareek <72460673+nimeshpareek@users.noreply.github.com>
2023-09-05 09:05:42 +0530

    adding tests

--- polls/views.py
+++ polls/views.py
@@ -41,7 +41,6 @@ class IndexView(generic.ListView):
 
 class DetailView(generic.DetailView):
     model = Question
-    
     template_name = "polls/detail.html"
 
 # def results(request, question_id):
